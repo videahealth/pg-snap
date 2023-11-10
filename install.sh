861a42d2ba113e86458148c893d086308651e62d
sharithg1 <sharith@videa.ai>
2023-11-10 13:49:28 -0500

    update install sh script

--- install.sh
+++ install.sh
@@ -11,18 +11,28 @@ TAG_NAME=$(echo "$LATEST_RELEASE" | sed -n 's/.*"tag_name": "\([^"]*\)".*/\1/p')
 ARCH=$(uname -m)
 
 if [ "$ARCH" = "arm64" ]; then
-    BINARY_NAME="pg-snap-aarch64-apple-darwin"
+    BINARY_ARCHIVE="pg-snap-v$TAG_NAME-aarch64-apple-darwin.tar.gz"
 elif [ "$ARCH" = "x86_64" ]; then
-    BINARY_NAME="pg-snap-x86_64-apple-darwin"
+    BINARY_ARCHIVE="pg-snap-v$TAG_NAME-x86_64-apple-darwin.tar.gz"
 else
     echo "Unsupported architecture: $ARCH"
     exit 1
 fi
 
-DOWNLOAD_URL="https://github.com/$REPO/releases/download/$TAG_NAME/$BINARY_NAME"
+DOWNLOAD_URL="https://github.com/$REPO/releases/download/$TAG_NAME/$BINARY_ARCHIVE"
 
-sudo curl -L -o /usr/local/bin/pg_snap "$DOWNLOAD_URL"
+curl -L -o "/tmp/$BINARY_ARCHIVE" "$DOWNLOAD_URL"
+tar -xzf "/tmp/$BINARY_ARCHIVE" -C /tmp
 
-sudo chmod +x /usr/local/bin/pg_snap
+BINARY_PATH="/tmp/pg-snap"
 
-sh -c 'pg_snap -h'
+if [ -f "$BINARY_PATH" ]; then
+    sudo mv "$BINARY_PATH" /usr/local/bin/pg_snap
+    sudo chmod +x /usr/local/bin/pg_snap
+    echo "pg_snap installed successfully."
+else
+    echo "Failed to find the pg_snap binary in the archive."
+    exit 1
+fi
+
+/usr/local/bin/pg_snap -h
