6914af3ea937967fe90b47fadbad12d575269197
Sharith Godamanna <sharithg@bu.edu>
2024-03-01 21:41:03 -0500

    remove comments

--- internal/relations/csv.go
+++ internal/relations/csv.go
@@ -7,17 +7,14 @@ import (
 )
 
 func ReadCSVColumnByName(filePath string, columnName string) ([]string, error) {
-	// Open the CSV file
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, fmt.Errorf("error opening CSV file: %w", err)
 	}
 	defer file.Close()
 
-	// Create a new CSV reader
 	reader := csv.NewReader(file)
 
-	// Read the first row to find the index of the column
 	header, err := reader.Read()
 	if err != nil {
 		return nil, fmt.Errorf("error reading CSV header: %w", err)
@@ -35,16 +32,13 @@ func ReadCSVColumnByName(filePath string, columnName string) ([]string, error) {
 		return nil, fmt.Errorf("column '%s' not found", columnName)
 	}
 
-	// Slice to hold values of the specific column
 	var columnData []string
 
-	// Read the CSV file
 	for {
 		record, err := reader.Read()
 		if err != nil {
-			break // Reached the end of the file
+			break
 		}
-		// Append the value of the specific column to the slice, if it exists
 		if len(record) > columnIndex {
 			columnData = append(columnData, record[columnIndex])
 		}
